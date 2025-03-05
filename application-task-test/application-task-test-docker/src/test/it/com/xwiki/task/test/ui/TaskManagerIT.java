de828c939c01ca919ab77b2ce45b58b01f20eab3
Teodor Caras <trrenty@gmail.com>
2025-03-05 10:13:25 +0200

    Make Tasks and Task report macros behavior more logical #170
    * Change the default behaviour of the task report macro to display tasks of all statuses.
    * Task report macro can now filter for multiple statuses.

--- application-task-test/application-task-test-docker/src/test/it/com/xwiki/task/test/ui/TaskManagerIT.java
+++ application-task-test/application-task-test-docker/src/test/it/com/xwiki/task/test/ui/TaskManagerIT.java
@@ -187,12 +187,13 @@ class TaskManagerIT
 
     @Test
     @Order(6)
-    void taskReport(TestUtils setup) {
+    void taskReport(TestUtils setup)
+    {
         setup.createPage(pageWithTaskRaport, TASK_REPORT_MACRO);
         ViewPageWithTasks viewPage = new ViewPageWithTasks();
         LiveTableElement taskReport = viewPage.getTaskReportLiveTable();
         taskReport.waitUntilReady();
-        assertEquals(2, taskReport.getRowCount());
+        assertEquals(3, taskReport.getRowCount());
         int taskTileCellIndex = taskReport.getColumnIndex("Task") + 1;
         int taskDeadlineCellIndex = taskReport.getColumnIndex("Deadline") + 1;
         int taskAssigneeCellIndex = taskReport.getColumnIndex("Assignee") + 1;
@@ -203,6 +204,11 @@ class TaskManagerIT
         assertEquals("", taskReport.getCell(row, taskAssigneeCellIndex).getText());
         assertEquals(pageWithTaskMacros.getName(), taskReport.getCell(row, taskLocationCellIndex).getText());
         row = taskReport.getRow(2);
+        assertEquals("#2\nDo this as well",
+            taskReport.getCell(row, taskTileCellIndex).getText());
+        assertEquals("-", taskReport.getCell(row, taskDeadlineCellIndex).getText());
+        assertEquals("", taskReport.getCell(row, taskAssigneeCellIndex).getText());
+        row = taskReport.getRow(3);
         assertEquals("#3\nDo this @Admin as late as 2023/01/01 12:00",
             taskReport.getCell(row, taskTileCellIndex).getText());
         assertEquals("01/01/2023 12:00:00", taskReport.getCell(row, taskDeadlineCellIndex).getText());
