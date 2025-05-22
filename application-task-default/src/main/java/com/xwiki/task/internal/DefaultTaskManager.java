91b48205dbaacce5558b9c263f26a6b9e6166e68
BRASSAT Alexandru <92524740+KebabRonin@users.noreply.github.com>
2025-05-22 09:23:12 +0300

    Add the possibility to define a task description template #94 (#214)
    * Added TaskTemplateList page
    * Removed automatic title and id generated for Template tasks
    * Added Create Template button
    * Move TaskManagerTemplate to TaskManagerTemplates space
    * Added task template section to admin section

--- application-task-default/src/main/java/com/xwiki/task/internal/DefaultTaskManager.java
+++ application-task-default/src/main/java/com/xwiki/task/internal/DefaultTaskManager.java
@@ -103,7 +103,7 @@ public class DefaultTaskManager implements TaskManager
             XWikiContext context = contextProvider.get();
             String statement = ", BaseObject as taskObj, IntegerProperty as idProp "
                 + "WHERE taskObj.name = doc.fullName "
-                + "AND doc.fullName != 'TaskManager.TaskManagerTemplate' "
+                + "AND doc.space != 'TaskManager.TaskManagerTemplates' "
                 + "AND taskObj.className = 'TaskManager.TaskManagerClass' "
                 + "AND taskObj.id = idProp.id.id AND idProp.id.name = 'number' "
                 + "AND idProp.value = :id";
